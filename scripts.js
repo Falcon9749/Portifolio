6f4009d492dfc0dea9e4b6ab39de3247524ac495
Falcon9749 <falcon.eletroinfo@gmail.com>
2024-05-21 17:24:38 -0300

    portifolio

---
+++ scripts.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const transitionElement = document.querySelector('.transition-fade');
+
+    // Remover a animação ao carregar a página
+    setTimeout(() => {
+        transitionElement.classList.add('fade-out');
+    }, 500);
+
+    function navigate(url) {
+        transitionElement.classList.remove('fade-out');
+
+        setTimeout(() => {
+            window.location.replace(url);
+        }, 500);
+    }
+
+    window.sobre = () => navigate('sobre.html');
+    window.projetos = () => navigate('projetos.html');
+    window.contatos = () => navigate('contatos.html');
+});
