6a78821fab67dcd83e54a40d5dc7809f82e3af3b
falcon9749 <falcon.eletroinfo@gmail.com>
2024-05-26 17:29:25 -0300

    Port1

---
+++ README.md
@@ -0,0 +1,1 @@
+Meu Potifólio
---
+++ scripts.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const transitionElement = document.querySelector('.page-transition');
+
+    // Adicionar animação de entrada ao carregar a página
+    setTimeout(() => {
+        transitionElement.style.transform = 'none';
+    }, 500);
+});
+
+function animateAndNavigate(targetUrl) {
+    const transitionElement = document.querySelector('.page-transition');
+    // const clickSound = document.getElementById('click-sound');
+
+
+    setTimeout(() => {
+        window.location.replace(targetUrl);
+    }, 300);// Tocar som de clique
+    clickSound.play();
+
+    console.log('Iniciando animação de virar página');
+    
+    // Adicionar classe para animação de virar a página
+    transitionElement.classList.add('page-flip');
+
+    // Esperar a animação antes de navegar
+    setTimeout(() => {
+        console.log('Navegando para:', targetUrl);
+        window.location.replace(targetUrl);
+    }, 800);
+}
+
+function sobre() {
+    animateAndNavigate('index.html');
+}
+
+function projetos() {
+    animateAndNavigate('projetos.html');
+}
+
+function contatos() {
+    animateAndNavigate('contatos.html');
+}
